b523853ed8d2527ee11719ac41842408f0a080d6
eraldo06 <eraldoleonardo384@gmail.com>
2022-12-11 01:14:16 -0300

    buscando usuario no github

--- componetes/Home.tsx
+++ componetes/Home.tsx
@@ -1,71 +1,92 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import styled from 'styled-components/native';
-
-const Input = styled.TextInput`
-    width: 80%;
-    background-color: black;
-    height: 50px;
-    border-radius: 10px;
-    align-content: center;
-    align-items: center;
-`;
-
-const ContainerBusca = styled.View`
-    height: 35%;
-    background-color: aqua;
-    flex-direction: row;
-`;
-const Lado01 = styled.View`
-    
-    height: 70%;
-    width: 60%;
-
-`
-const Lado02 = styled.View`
-    
-    height: 70%;
-    width: 40%;
-`
-
-const ContainerResultado = styled.View`
-    flex: 1;
-    background-color: #3a4141;
-`;
+import { ContainerBusca, Div, Lado01, Lado02, Input, ContainerResultado, TextoResul, Buttoon } from './componetes'
 
 
+
+
+let linha = false
 const Home = () => {
+
+    const [usario, setUsuario] = useState([]);
+
+    const carregar = async () => {
+        const req = await fetch(`https://api.github.com/users/${name}`);
+        const json = await req.json();
+
+        if (json) {
+            setUsuario(json)
+        }
+        linha = true
+      
+    }
+
+    const [name, setName] = useState('')
+    const [mostrar, setMostrar] = useState(false)
+
+    function mudarTexto(texto: any) {
+        setName(texto)
+        if(name.length > 1){
+            setMostrar(true)
+          }else{
+       
+          }
+    }
     return (
         <>
             <ContainerBusca>
-                
+                <Div>
                     <Lado01>
                         <Text></Text>
-                        <Text></Text>
-                        <Text style={{fontSize:35}}>HUBusca</Text>
-                        <Text style={{fontSize:17}}>Encrotre os melhores</Text>
-                        <Text style={{fontSize:17}}>repositórios no github</Text>
+                        <Text style={{ fontSize: 35 }}>HUBusca</Text>
+                        <Text style={{ fontSize: 16 }}>Encrotre os melhores</Text>
+                        <Text style={{ fontSize: 16 }}>repositórios no github</Text>
                     </Lado01>
-
                     <Lado02>
-                        
                         <Image
-                        source={require('../assets/logi.png')}
-                        style={{ width: 100, height: 150 }}
+                            source={require('../assets/logi.png')}
+                            style={{ width: 100, height: 150 }}
                         />
                     </Lado02>
-               
+                </Div>
+
+                <Input onChangeText={mudarTexto} />
+                <Buttoon onPress={carregar}>Carregar</Buttoon>
+
+
+
             </ContainerBusca>
 
             <ContainerResultado>
+                <TextoResul>
+                    {mostrar === false &&
+                        <Text style={{ fontSize: 20 }}>Nenhum historico...</Text>
+                    }
+                    {linha &&
+                        <Div style={{backgroundColor:'red'}}>
+                        <Image source={{ uri: `${usario.avatar_url}` }}
+                            style={{ width: 80, height: 80, borderRadius:20 }}
+                        />
+                        <View>
+                            <Text>{usario.login}</Text>
+                            <Text>{usario.name}</Text>
+                            <Text>{usario.id}</Text>
+                            <Text>{usario.location}</Text>
+                        </View>
+                    </Div>
+                    }
+                    
 
+
+
+
+
+                </TextoResul>
             </ContainerResultado>
         </>
     )
 }
-
-
-
 
 export default Home;
---
+++ componetes/componetes.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import styled from 'styled-components/native';
+
+export const Input = styled.TextInput`
+    width: 80%;
+    background-color: #3a4141;
+    color: white;
+    height: 50px;
+    border-radius: 10px;
+   margin-left: 8%;
+    margin-top: 15%;
+`;
+
+export const ContainerBusca = styled.View`
+    height: 55%;
+    background-color: aqua;
+`;
+export const Div = styled.View`
+    flex-direction: row;
+    margin-top: 10%;
+    margin-left: 5%;
+`;
+export const Lado01 = styled.View`
+    height: 70%;
+    width: 60%;
+`
+export const Lado02 = styled.View`
+    height: 70%;
+    width: 40%;
+`
+
+// Resultados
+export const ContainerResultado = styled.View`
+    flex:1;
+    background-color: #3a4141;
+`;
+
+export const TextoResul = styled.SafeAreaView`
+  justify-content: center;
+  align-items: center;
+  margin-top: 20%;
+`
+
+export const Buttoon = styled.Text`
+        background-color: black;
+        width:80%;
+        margin-left: 8%;
+        height: 50px;
+        color: white;
+        border-radius: 15px;
+        
+`
